4c3fa7f280ad7dae1e07dd5ca9303dbda694057f
vasya <toshamatveeev@gmail.com>
2017-05-27 13:43:02 +0200

    Go

---
+++ app.js
@@ -0,0 +1,26 @@
+//Parsing elements
+var aboutMe = document.getElementsByTagName('ul');
+var nickName = document.getElementById('nickname');
+var favorites = document.getElementById('favorites');
+var homeTown = document.getElementById('hometown');
+var listItems = document.getElementsByTagName('li');
+
+//Document styling
+document.body.style.backgroundColor = "#ff0000";
+document.body.style.fontFamily = "sans-serif";
+aboutMe[0].style.backgroundColor = "#3c40ff";
+
+//Adding text
+nickname.innerHTML = "yozhikvtumane";
+favorites.innerHTML = "a lot";
+homeTown.innerHTML = "New-York (jk)";
+
+//Adding class to listitems
+for (var i = 0; i < listItems.length; i++) {
+	listItems[i].className = "listitem";
+}
+
+//Creating  and adding Image
+var image = document.createElement('img');
+image.src = "http://lorempixel.com/200/200/cats/1";
+aboutMe[0].appendChild(image);
